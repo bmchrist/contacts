feb3b7b2048a00f982f9272c70058bb4e6b75c95
Ben Christensen <ben@joinhandshake.com>
2018-07-18 14:25:32 -0400

    Properly list and clear selected information

--- app/src/main/java/com/example/ben/contacts/CreateNoteActivity.java
+++ app/src/main/java/com/example/ben/contacts/CreateNoteActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.CursorAdapter;
 import android.widget.EditText;
@@ -24,14 +25,16 @@ public class CreateNoteActivity extends AppCompatActivity {
         // Pretty sure cursor can be null here since we use setFilterQueryProvider below
         final CursorAdapter adapter = new ContactListAdapter(CreateNoteActivity.this, null, 0);
 
-        // Set it to filter the contacts query absed on the input constraint from the text field
+        // Set it to filter the contacts query based on the input constraint from the text field
         adapter.setFilterQueryProvider(new FilterQueryProvider() {
             public Cursor runQuery(CharSequence constraint) { return getContactsCursor(constraint); }
         });
 
         // and apply it
+        // TODO(bmchrist): is there a selector that is better than text (eg drowpdown w/ search?)
+        // if not, decide how to handle invalid text and make it clear it won't be used
         AutoCompleteTextView textView = findViewById(R.id.editNoteContact);
-        // textView.setOnItemClickListener(onItemClickListener);
+        textView.setOnItemClickListener(onItemClickListener); // todo decide which is better);
         textView.setAdapter(adapter);
     }
 
@@ -40,24 +43,25 @@ public class CreateNoteActivity extends AppCompatActivity {
         String noteContact = ((EditText) findViewById(R.id.editNoteContact)).getText().toString();
         Log.d(TAG, noteBody);
         Log.d(TAG, noteContact);
-        Log.e(TAG, "TODO: implement")
+        Log.e(TAG, "TODO: implement");
     }
 
-//    // TODO(bmchrist): This feels janky - look for better options once I know more
-//    private AdapterView.OnItemClickListener onItemClickListener =
-//        new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-////                Cursor cursor = (CursorWrapper) adapterView.getItemAtPosition(i);
-////                String name = cursor.getString( cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-////                String lookup = cursor.getString( cursor.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
-//                String name = "";
-//                String lookup = "";
-//                Log.d(TAG,"Clicked item from auto completion list "  + name + " lookup: " + lookup);
-////                ((EditText) findViewById(R.id.editNoteContact)).setText("test"+name);
-//
-//            }
-//        };
+    // TODO(bmchrist): This feels janky - look for better options once I know more
+    private AdapterView.OnItemClickListener onItemClickListener =
+        new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Cursor cursor = (Cursor) adapterView.getItemAtPosition(position);
+                String name = cursor.getString( cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                String lookup = cursor.getString( cursor.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));;
+                Log.d(TAG,"Clicked item from auto completion list "  + name + " lookup: " + lookup);
+                // TODO(bmchrist): Store th contact somewhere
+                // Clear out the entry so we can add more
+                ((EditText) findViewById(R.id.editNoteContact)).setText("");
+
+            }
+        };
+
 
     private Cursor getContactsCursor(CharSequence constraint) {
         ContentResolver cr = getContentResolver();
